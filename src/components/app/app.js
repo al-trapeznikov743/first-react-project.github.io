0f81e25951512044ef502940aa5bf3cafa1e5d90
al.t <al.trapeznikov.743@gmal.com>
2020-07-26 18:54:39 +0500

    add 'important' and 'like'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,14 +7,21 @@ import PostAddForm from '../post-add-form'
 import '../app/app.css'
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 1},
+        {label: 'That is so good', important: false, id: 2},
+        {label: 'I need a break', important: false, id: 3}
+    ];
+
     return (
-        <div className = "app">
+        <div className = 'app'>
             <AppHeader/>
-            <div className = "search-panel d-flex">
+            <div className = 'search-panel d-flex'>
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts = {data}/>
             <PostAddForm/>
         </div>
     )
